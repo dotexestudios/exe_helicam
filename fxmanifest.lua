557afb0d86cd2c724dad796e9d97ad66cbe8823d
filo <kloud.stu@gmail.com>
2026-02-19 04:58:48 +0800

    v1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,23 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+original_author "ZeroFour04"
+author ".exe Studios (filo)"
+description "A FiveM helicoper camera script"
+discord "https://discord.gg/EWKWXVBHK7"
+
+version "1.0.0"
+
 shared_scripts {
     "@ox_lib/init.lua",
     "config.lua",
 }
 
-client_script "client.lua"
-server_script "server.lua"
+server_scripts {
+    "version_check.lua",
+    "server.lua",
+}
+
+client_scripts {
+    "client.lua",
+}
---
+++ version_check.lua
@@ -0,0 +1,48 @@
+local color = "^4"
+local white = "^7"
+local logo = [[
+                                     ▄▄▄▄    ▄               █    ▀
+         ▄▄▄   ▄   ▄   ▄▄▄          █▀   ▀ ▄▄█▄▄  ▄   ▄   ▄▄▄█  ▄▄▄     ▄▄▄    ▄▄▄
+        █▀  █   █▄█   █▀  █         ▀█▄▄▄    █    █   █  █▀ ▀█    █    █▀ ▀█  █   ▀
+        █▀▀▀▀   ▄█▄   █▀▀▀▀             ▀█   █    █   █  █   █    █    █   █   ▀▀▀▄
+   █    ▀█▄▄▀  ▄▀ ▀▄  ▀█▄▄▀         ▀▄▄▄█▀   ▀▄▄  ▀▄▄▀█  ▀█▄██  ▄▄█▄▄  ▀█▄█▀  ▀▄▄▄▀
+]]
+
+
+local currentVersion = GetResourceMetadata(cache.resource, 'version', 0)
+local githubRepo = "dotexestudios/exe_helicam"
+
+local function checkVersion()
+    print(color .. logo .. white)
+    PerformHttpRequest("https://raw.githubusercontent.com/" .. githubRepo .. "/master/version", function(err, newestVersion, headers)
+        if newestVersion then
+            newestVersion = newestVersion:gsub("%s+", "")
+            if newestVersion ~= currentVersion then
+                print("\n^1-------------------------------------------------------")
+                print(("^1[Update Available] ^7%s"):format(resourceName))
+                print(("^3Current: ^7%s | ^2Latest: ^7%s"):format(currentVersion, newestVersion))
+                print(("^3Download: ^7https://github.com/%s"):format(githubRepo))
+                print("^1-------------------------------------------------------\n")
+            else
+                print(("^2[Success] ^7%s is up to date (v%s)"):format(resourceName, currentVersion))
+            end
+        else
+            print(("^1[Error] ^7Could not check version for %s"):format(resourceName))
+        end
+    end, "GET", "", "")
+end
+
+AddEventHandler("onResourceStart", function(resource)
+    if resource ~= cache.resource then return end
+    Wait(5000)
+    checkVersion()
+end)
+
+CreateThread(function()
+    if cache.resource ~= "exe_helicam" then
+        while true do
+            Wait(5000)
+            print("Cannot check version for exe_helicam, make sure you are using the correct resource name")
+        end
+    end
+end)
